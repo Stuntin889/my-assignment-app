48217cf1e913be2f869354c42b24f174327c04f8
Imaekhai Oshiokhai <i.oshiokhai733@mybvc.ca>
2026-04-02 00:49:57 -0600

    Initial React app with name display and unit test

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Imaekhai Christopher</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+
+test('shows my name on the page', () => {
+  render(<App />);
+  expect(screen.getByText('Imaekhai Christopher')).toBeInTheDocument();
+});
